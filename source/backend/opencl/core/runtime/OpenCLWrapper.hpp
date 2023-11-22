171230be5aa50ffdc03de976aa579cb3eabee02f
Ger Hobbelt <ger@hobbelt.com>
2023-11-22 11:50:06 +0100

    unify the code to default to using openCL 1.2 when no version has been specified. We use OpenCL 1.2 as the default as that SHOULD work okay on non-Intel+NVIDIA hardware (AMD, for example) and we want a multi-platform compatible build by default.

--- source/backend/opencl/core/runtime/OpenCLWrapper.hpp
+++ source/backend/opencl/core/runtime/OpenCLWrapper.hpp
@@ -18,7 +18,7 @@
 #include <memory>
 #include "core/Macro.h"
 #define CL_TARGET_OPENCL_VERSION 200
-#define CL_HPP_TARGET_OPENCL_VERSION 110
+#define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_MINIMUM_OPENCL_VERSION 110
 
 #if !defined(_MSC_VER)
